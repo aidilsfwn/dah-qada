8e126346f5e5f48aeda41967c743c926c17d6016
Aidil Safwan <aidilsfwn@icloud.com>
2025-04-07 01:11:21 +0800

    remove chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { Flex, Box, Heading } from "@chakra-ui/react";
-
-import { ColorModeButton } from "@/components/ui/color-mode";
-
-function App() {
+const App = () => {
   return (
     <>
-      <Box position="sticky" top="0" zIndex="10" boxShadow="md" p={4}>
-        <Flex justify="space-between" align="center">
-          <Heading as="h1" size="lg">
-            Dah Qada?
-          </Heading>
-          <Flex>
-            <ColorModeButton />
-          </Flex>
-        </Flex>
-      </Box>
+      <p>eric cantona</p>
     </>
   );
-}
+};
 
 export default App;
